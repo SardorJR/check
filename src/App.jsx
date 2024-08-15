94b406682bc19fd8d0ad9ef452769080894c723f
Sardor <sardorjn2006@gmail.com>
2024-08-16 00:04:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+
+function App() {
+  return (
+   <>
+<div className="v"></div>
+   </>
+  )
+}
+
+export default App;
+
